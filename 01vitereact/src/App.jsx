7af9206b3bf44e3d1931a9778a58290172b40d45
Karan Singh <karans21.dev@gmail.com>
2025-01-26 12:02:23 +0530

    Understanding of the components

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,13 @@
+import Test from "../test"
+
+function App() {
+  return (
+    <>
+    <Test />
+    <h1>We aer ready for react.js</h1>
+    <span><p style={{color:"orange",fontSize:'50px'}}>Radheshyyam!</p></span>
+    </>
+  )
+}
+
+export default App
---
+++ 01vitereact/test.jsx
@@ -0,0 +1,10 @@
+function Test() {
+    return(
+        <div>
+            <h1>Hello World!</h1>
+            <p style={{fontSize:'50px'}}>This is a test component.</p>
+        </div>
+    )
+}
+
+export default Test;
