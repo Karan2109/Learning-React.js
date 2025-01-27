c29910c77a4a97d08f8f7e3f902e1c92e57accf1
Karan Singh <karans21.dev@gmail.com>
2025-01-27 10:43:10 +0530

    Basic updation & learning of react

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -4,8 +4,6 @@ function App() {
   return (
     <>
     <Test />
-    <h1>We aer ready for react.js</h1>
-    <span><p style={{color:"orange",fontSize:'50px'}}>Radheshyyam!</p></span>
     </>
   )
 }
--- 01vitereact/test.jsx
+++ 01vitereact/test.jsx
@@ -2,7 +2,7 @@ function Test() {
     return(
         <div>
             <h1>Hello World!</h1>
-            <p style={{fontSize:'50px'}}>This is a test component.</p>
+            <p>This is a test component.</p>
         </div>
     )
 }
